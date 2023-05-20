8a64804525364640d87e998abae31b8d68bd1502
m-emadeddin <mohamed.emad12363@gmail.com>
2023-05-20 16:48:30 +0300

    Update Database

--- college.sql
+++ college.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2023 at 09:03 PM
+-- Generation Time: May 20, 2023 at 03:48 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,21 +24,76 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login-info`
+-- Table structure for table `dept_info`
 --
 
-CREATE TABLE `login-info` (
+CREATE TABLE `dept_info` (
+  `id` varchar(20) NOT NULL,
+  `nid` varchar(14) NOT NULL,
+  `dept` varchar(50) NOT NULL,
+  `year` varchar(50) NOT NULL,
+  `sem` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `dept_info`
+--
+
+INSERT INTO `dept_info` (`id`, `nid`, `dept`, `year`, `sem`) VALUES
+('std_1', '12345678901234', 'Computer and Systems', 'Third', 'Second'),
+('std_2', '12341234567890', 'Embedded Systems', 'Third', 'Second'),
+('std_3', '12341234567666', 'Flutter Dev', 'Third', 'Second');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
   `username` varchar(20) NOT NULL,
   `password` text NOT NULL,
   `is_admin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
--- Dumping data for table `login-info`
+-- Dumping data for table `login`
 --
 
-INSERT INTO `login-info` (`username`, `password`, `is_admin`) VALUES
-('emadeddin', 'asdasdsadasda', 1);
+INSERT INTO `login` (`username`, `password`, `is_admin`) VALUES
+('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 1),
+('emadeddin', 'bf1b9ea2662573a87dd2070728c9b009e627e049', 1),
+('std_1', '33615028dc041431e7ef4249a237d90ea9da3355', 0),
+('std_2', '23bffce445d3046c7403d15a46502f4246a0b197', 0),
+('std_3', 'b010ed3f99f383465ace0100356c874bfee4c555', 0),
+('user', '12dea96fec20593566ab75692c9949596833adc9', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personal_info`
+--
+
+CREATE TABLE `personal_info` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(11) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `birthday` date NOT NULL,
+  `national` varchar(50) NOT NULL,
+  `account` varchar(50) NOT NULL,
+  `pic` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `personal_info`
+--
+
+INSERT INTO `personal_info` (`id`, `name`, `email`, `phone`, `address`, `birthday`, `national`, `account`, `pic`) VALUES
+('std_1', 'Muhammed Emadeddin', 'muhammed.emadeddin@gmail.com', '01102114701', 'Shershiema, Hehia, Egypt', '2001-07-23', 'Egyptian', 'muhammed.emadeddin@zu.edu.eg', '8.JPG'),
+('std_2', 'Ahmed Atef', 'ahmed.atef@gmail.com', '01010101001', 'Zagazig, Egypt', '2001-01-01', 'Egyptian', 'ahmed.atef@zu.edu.eg', 'any.jpg'),
+('std_3', 'Moaz Blzen', 'moza.blzen@gmail.com', '01234567890', 'Zagazig, Egypt', '2001-12-12', 'Egyptian', 'moza.blzen@zu.edu.eg', 'any.jpg');
 
 -- --------------------------------------------------------
 
@@ -58,28 +113,35 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`id`, `name`, `score`, `avatar`) VALUES
-('0154313', 'Ahmed Atef', 0, 'admin.jpg'),
-('1', 'Muhammed Emadeddin', 50, 'admin.jpg'),
-('1036556576', 'jOmUJFLvpF', 150, 'GYsvBkuKwK.jpg'),
-('1995165452', 'VTMpFInZEQ', 404, 'snmyZcpdNs.jpg'),
-('3885143567', 'zxRIraTeuM', 402, 'VzQcbSVFhJ.jpg'),
-('4962153797', 'MtSKOMfOJH', 220, 'znKAEENVDj.jpg'),
-('6262726063', 'DdwgcIeAsF', 346, 'XpPRzpacgn.jpg'),
-('7388532899', 'EfSPNcBzSo', 425, 'yoWOFVFGKi.jpg'),
-('7415067037', 'WEhpFGoqQj', 165, 'loZNOziMWN.jpg'),
-('8253254308', 'xkGuQwveiH', 41, 'IDAvzkCEfM.jpg'),
-('8598199894', 'iLEVjcrGAd', 109, 'hVaxybMdSU.jpg'),
-('9822839375', 'UpXzxoCSwk', 269, 'rhEPJwpVGJ.jpg');
+('1', 'Muhammed Emadeddin', 64, 'admin.jpg'),
+('2', 'user', 7, 'any.jpg'),
+('53f30211', 'CARD3', 1, 'CARD3.JPG'),
+('9355b592', 'CARD1', 3, 'CARD1.JPG'),
+('std_1', 'Muhammed Emadeddin', 7, 'any.jpg'),
+('std_2', 'Ahmed Atef', 2, 'any.jpg'),
+('std_3', 'Moaz Ahmed', 2, 'any.jpg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `login-info`
+-- Indexes for table `dept_info`
 --
-ALTER TABLE `login-info`
+ALTER TABLE `dept_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `personal_info`
+--
+ALTER TABLE `personal_info`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `student`
