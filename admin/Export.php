0d253c9fff71cdf562c516ab3eafcc1bbc41b11d
AhmedAtef <aatef83156@gmail.com>
2023-05-20 16:23:53 +0300

    Now , User Can Download CSV File

--- admin/Export.php
+++ admin/Export.php
@@ -7,12 +7,11 @@ $query->setFetchMode(PDO::FETCH_ASSOC);
 $rows = $query->fetchAll();
 
 // Create a file handle
-$file = fopen('export.csv', "w");
+$file = fopen('export.csv', 'w');
 
-// Write the CSV headers if the file is newly created
+// Write the CSV headers
 $headers = ['ID', 'Name', 'Avatar', 'Attendance No.'];
 fputcsv($file, $headers);
-
 
 // Write the data rows
 foreach ($rows as $row) {
@@ -22,4 +21,12 @@ foreach ($rows as $row) {
 // Close the file handle
 fclose($file);
 
+// Set the appropriate headers for file download
+header('Content-Type: text/csv');
+header('Content-Disposition: attachment; filename="export.csv"');
+
+// Send the file content to the browser
+readfile('export.csv');
+unlink("export.csv");
+exit();
 ?>
--- admin/index.php
+++ admin/index.php
@@ -197,11 +197,12 @@ $total_lectures = 750;
                                 </div>
                             </div> <!-- /.card -->
                             <div class=" d-flex justify-content-end bg-transparent border-white">
-                                <form action="export.php">
-                                    <button type="button" class="btn btn-outline-primary ms-auto"
+                            <form action="export.php">
+                                    <button type="submit" class="btn btn-outline-primary ms-auto"
                                         onclick="To_Excel()">Export</button>
-                                </form>
-                        </div>  <!-- /.col-lg-8 -->
+                            </form>
+
+                            </div>  <!-- /.col-lg-8 -->
                         <!-- /.col-md-4 -->
                     </div>
                 </div>
